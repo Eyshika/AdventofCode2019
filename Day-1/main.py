7ab00116a09a303185a982464f76e65d3ca528f6
Eyshika agarwal <eyshikaagarwal@eyshikas-mbp.home>
2019-12-01 19:29:41 -0800

    Final Day 1

--- Day-1/main.py
+++ Day-1/main.py
@@ -1,11 +1,14 @@
+## Day - 1
 
 def evaluate(mass):
-    #Part - 1
-    fuel = 0
-    fuel = int(mass/3) 
-    if fuel > 0:
-        fuel = fuel - 2
-    return fuel
+    newfuel = 0
+    fuel = int(mass/3) - 2
+    while fuel > 0:
+        newfuel += fuel
+        fuel = int(fuel/3) - 2
+        print(fuel)
+        
+    return newfuel
 
 if __name__ == '__main__':
     module = open("input.txt", 'r')
